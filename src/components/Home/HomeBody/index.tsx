91f839062ff985d10f6a9f0e73bd5b98702fb7f2
Lcbzinho <bassanilucas055@gmail.com>
2025-12-08 00:03:00 -0300

    Att 0.7.2 - Refatoring Home

--- src/components/Home/HomeBody/index.tsx
+++ src/components/Home/HomeBody/index.tsx
@@ -1,45 +1,16 @@
+import { useGetRestaurantsQuery } from '../../../store/api/api'
 import { Card } from '../../SubComponents/Card'
 import { HomeBodyContainer, BodyContainer, Loading } from './styles'
-import { useEffect, useState } from 'react'
-
-type RestaurantType = {
-  id: number
-  titulo: string
-  descricao: string
-  tipo: string
-  capa: string
-  avaliacao: number
-  cardapio: {
-    id: number
-    nome: string
-    descricao: string
-    foto: string
-    preco: number
-    porcao: string
-  }[]
-}
 
 export const Body = () => {
-  const [restaurants, setRestaurants] = useState<RestaurantType[]>([])
-  const [carregando, setCarregando] = useState(true)
-
-  useEffect(() => {
-    async function load() {
-      const answer = await fetch('https://api-ebac.vercel.app/api/efood/restaurantes')
-      const date = await answer.json()
-      setCarregando(false)
-      setRestaurants(date)
-    }
-
-    load()
-  }, [])
+  const { data, isLoading } = useGetRestaurantsQuery()
 
   return (
     <BodyContainer>
       <div className="container">
-        {carregando ? <Loading>{<h1>Loading...</h1>}</Loading> : ''}
+        {isLoading ? <Loading>{<h1>Loading...</h1>}</Loading> : ''}
         <HomeBodyContainer>
-          {restaurants.map((RestaurantItem) => {
+          {data?.map((RestaurantItem) => {
             return (
               <Card
                 image={RestaurantItem.capa}
